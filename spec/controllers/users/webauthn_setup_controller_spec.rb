1d7a2ee6d3d8fca6b63a53fe5536a19913cf3cbf
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-10-05 01:46:41 -0400

    Misc

--- spec/controllers/users/webauthn_setup_controller_spec.rb
+++ spec/controllers/users/webauthn_setup_controller_spec.rb
@@ -68,10 +68,10 @@ describe Users::WebauthnSetupController do
         controller.user_session[:webauthn_challenge] = challenge
       end
 
-      it 'processes a valid webauthn and redirects to account page' do
+      it 'processes a valid webauthn and redirects to success page' do
         patch :confirm, params: params
 
-        expect(response).to redirect_to(webauthn_success_url)
+        expect(response).to redirect_to(webauthn_setup_success_url)
       end
 
       it 'tracks the submission' do
@@ -134,10 +134,10 @@ describe Users::WebauthnSetupController do
         controller.user_session[:webauthn_challenge] = challenge
       end
 
-      it 'processes a valid webauthn and redirects to personal key page' do
+      it 'processes a valid webauthn and redirects to success page' do
         patch :confirm, params: params
 
-        expect(response).to redirect_to(sign_up_personal_key_url)
+        expect(response).to redirect_to(webauthn_setup_success_url)
       end
     end
   end
