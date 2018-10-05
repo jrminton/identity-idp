a1b4e25ad99ade5bdf144c392a33be81d33a0f52
James Smith <jamesg.smith@gsa.gov>
2018-10-05 12:52:58 -0400

    load webauthn configs if not loaded

--- app/models/concerns/webauthn_configurations_extension.rb
+++ app/models/concerns/webauthn_configurations_extension.rb
@@ -1,6 +1,7 @@
 module WebauthnConfigurationsExtension
   # :reek:FeatureEnvy
   def selection_presenters
+    proxy_association.load_target unless proxy_association.loaded?
     configuration = proxy_association.target.detect(&:mfa_enabled?)
 
     if configuration.present?
